f9eb1c5ad3dbdc975961276b676f1b00ab3d3265
cyremann <jesusbassist@gmail.com>
2017-11-17 21:10:11 -0600

    Routine Work

--- rocketkey/urls.py
+++ rocketkey/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^keymanage/', include('keymanage.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'', include('keymanage.urls'))
 ]
